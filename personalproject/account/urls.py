fc6286dbd0307f12222a4abedee13ce5baeff642
Toan <crisriorock0@gmail.com>
2022-06-23 15:27:04 +0700

    update readme

--- personalproject/account/urls.py
+++ personalproject/account/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('login/', views.MyTokenObtainPairView.as_view(), name='login'),
     path('logout/', TokenBlacklistView.as_view(), name='logout'),
     path('profile/', views.UpdateAndDetailProfileView.as_view(), name='profile'),
-    path('profile/mode/', views.UpdateProfileView.as_view(), name='update_profile'),
+    path('profile/mode/', views.UpdateProfileView.as_view(), name='update_mode'),
+    path('profile/change-password/', views.ChangePasswordView.as_view(), name='change_password'),
     path('refresh-token/', views.TokenRefreshView.as_view(), name='refresh_token'),
-    path('forgot-password-send-mail/', views.ForgotPassword, name='forgot_password'),
-    path('change-password/', views.ChangePasswordView.as_view(), name='change_password'),
+    path('forgot-password-send-mail/', views.ForgotPassword, name='forgot_password'),    
     path('change-password-with-token/', views.ChangePasswordWithTokenView.as_view(), name='change_password_with_token'),
 ]
